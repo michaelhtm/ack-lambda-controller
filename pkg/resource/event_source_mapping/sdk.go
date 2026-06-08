380abe9cc100bf6b1b084a89a68bdcd033ba3e3b
Mallikarjunadevops <mallikarjunadevops@example.com>
2026-06-08 16:24:00 -0400

    chore(lambda): remove redundant sdk_update_pre_build_request hook
    Addresses review comment in #220.
    Since the DestinationConfig is sanitized on all outputs (ReadOne/Create),
    the empty struct is never persisted to the desired spec, making the pre-update
    sanitization redundant.

--- pkg/resource/event_source_mapping/sdk.go
+++ pkg/resource/event_source_mapping/sdk.go
@@ -991,17 +991,6 @@ func (rm *resourceManager) sdkUpdate(
 	defer func() {
 		exit(err)
 	}()
-	if desired.ko.Spec.DestinationConfig != nil {
-		if desired.ko.Spec.DestinationConfig.OnFailure != nil && desired.ko.Spec.DestinationConfig.OnFailure.Destination == nil {
-			desired.ko.Spec.DestinationConfig.OnFailure = nil
-		}
-		if desired.ko.Spec.DestinationConfig.OnSuccess != nil && desired.ko.Spec.DestinationConfig.OnSuccess.Destination == nil {
-			desired.ko.Spec.DestinationConfig.OnSuccess = nil
-		}
-		if desired.ko.Spec.DestinationConfig.OnFailure == nil && desired.ko.Spec.DestinationConfig.OnSuccess == nil {
-			desired.ko.Spec.DestinationConfig = nil
-		}
-	}
 	input, err := rm.newUpdateRequestPayload(ctx, desired, delta)
 	if err != nil {
 		return nil, err
